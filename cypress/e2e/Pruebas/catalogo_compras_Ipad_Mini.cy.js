2e7e85cc37531e7b45e37fe14264a0abcd87d234
Othon Gasca <angeloga1973@gmail.com>
2024-08-10 13:42:07 -0600

    Se crea test para Ipad Mini

--- cypress/e2e/Pruebas/catalogo_compras_Ipad_Mini.cy.js
+++ cypress/e2e/Pruebas/catalogo_compras_Ipad_Mini.cy.js
@@ -101,91 +101,92 @@ describe(CommonPageData.testSuites.catalogoYCompras, () =>{
 
    
 
-      it.only('Realizar una compra', ()=>{
-   
-         Logger.NumerodePaso(1)
-         Logger.step('Iniciar sesión como usuario registrado.')
-         Logger.subStep('Navegar a la página demo')
-         CommonPageMetodos.navegarEnMenus();
-         Logger.subStep('Click en Link "Log In" ')
-         CommonPageMetodos.clickMenuLogin();
-         LoginMetod.login(usuario.usuariovalido, usuario.contrasenavalida)
+      
+      it.only('Realizar una compra Ipad Mini', ()=>{
+        // Ipad Air ancho 768 x 1024
+        
+
+        Logger.NumerodePaso(1)
+        Logger.step('Iniciar sesión como usuario registrado. Ipad Mini')
+        Logger.subStep('Navegar a la página demo Ipad Mini')
+        CommonPageMetodos.navegarEnMenus();
+        cy.viewport(768, 1024)
+        Logger.subStep('Click en Link "Log In Ipad Mini" ')
+        CommonPageMetodos.clickMenuLogin();
+        LoginMetod.login(usuario.usuariovalido, usuario.contrasenavalida)
 
 
-         Logger.NumerodePaso(2)
-         Logger.step('Navegar a la página de inicio')
-         CommonPageMetodos.clickMenuHome();
+        Logger.NumerodePaso(2)
+        Logger.step('Navegar a la página de inicio Ipad Mini')
+        CommonPageMetodos.clickMenuHome();
+
+       
+        Logger.NumerodePaso(3)
+        Logger.step('Seleccionar una cateroría de productos en el menú de navegación Ipad Mini') 
+        HomeMetodos.clickOpcionMonitor();
 
         
-         Logger.NumerodePaso(3)
-         Logger.step('Seleccionar una cateroría de productos en el menú de navegación') 
-         HomeMetodos.clickOpcionMonitor();
+        Logger.NumerodePaso(4)
+        Logger.step('Hacer clic en un producto específico Ipad Mini')
+        HomeMetodos.clickEnProducto(producto)
+        
+        Logger.NumerodePaso(5)
+        Logger.step('Verificar que se muestra la página de detalle del producto Ipad Mini')
+        DetalleProductoMetodo.verificaMuestrePaginaDetalleProducto();
 
-         
-         Logger.NumerodePaso(4)
-         Logger.step('Hacer clic en un producto específico')
-         HomeMetodos.clickEnProducto(producto)
-         
-         Logger.NumerodePaso(5)
-         Logger.step('Verificar que se muestra la página de detalle del producto')
-         DetalleProductoMetodo.verificaMuestrePaginaDetalleProducto();
+        Logger.NumerodePaso(6)
+        Logger.step('Hacer clic en boton "Agregar a carrito" Ipad Mini')
+        DetalleProductoMetodo.clickBotonAgregarCarrito();
 
-         Logger.NumerodePaso(6)
-         Logger.step('Hacer clic en boton "Agregar a carrito"')
-         DetalleProductoMetodo.clickBotonAgregarCarrito();
+        
+        Logger.NumerodePaso(7)
+        Logger.verificacion('Vefificar que ese muestra un mensaj ede confirmacion y el producto se agrega al carrito Ipad Mini')
+        DetalleProductoMetodo.verificarMensajeProductoAgregado();
+        CommonPageMetodos.clickMenuCart();
+        carritoMetodos.verificaProductoAgregadoAlCarrito(producto) 
+        
+        
+        Logger.NumerodePaso(8)
+        Logger.step('Hacer clic en el menú "Car" de la barra de navegación Ipad Mini')
+        CommonPageMetodos.clickMenuCart();
 
-         
-         Logger.NumerodePaso(7)
-         Logger.verificacion('Vefificar que ese muestra un mensaj ede confirmacion y el producto se agrega al carrito')
-         DetalleProductoMetodo.verificarMensajeProductoAgregado();
-         CommonPageMetodos.clickMenuCart();
-         carritoMetodos.verificaProductoAgregadoAlCarrito(producto) 
-         
-         
-         Logger.NumerodePaso(8)
-         Logger.step('Hacer clic en el menú "Car" de la barra de navegación')
-         CommonPageMetodos.clickMenuCart();
-
-         Logger.NumerodePaso(9)
-         Logger.step('Verificar que se muestra la página del carrito de compras')
-         carritoMetodos.verificaMuetraPaginaCarritoPorLaURL();
+        Logger.NumerodePaso(9)
+        Logger.step('Verificar que se muestra la página del carrito de compras Ipad Mini')
+        carritoMetodos.verificaMuetraPaginaCarritoPorLaURL();
 
 
-         Logger.NumerodePaso(10)
-         Logger.step('Hacer clic en el botón "Place Order"')
-         carritoMetodos.clickEnBotonPleceOrder();
+        Logger.NumerodePaso(10)
+        Logger.step('Hacer clic en el botón "Place Order" Ipad Mini')
+        carritoMetodos.clickEnBotonPleceOrder();
 
 
-         Logger.NumerodePaso(11)
-         Logger.step('Completar los campos obligatorios en la página de informacion de envío')
-         cy.wait(3000)
-         PlaceOrderMetodos.insertaInformacionDeOrden(PlaceOrderData.testData)
+        Logger.NumerodePaso(11)
+        Logger.step('Completar los campos obligatorios en la página de informacion de envío Ipad Mini')
+        cy.wait(3000)
+        PlaceOrderMetodos.insertaInformacionDeOrden(PlaceOrderData.testData)
 
-         Logger.NumerodePaso(12)
-         Logger.step('Hacer clic en el botón "Purchase"')
-         PlaceOrderMetodos.clickOnPurchaseButton();
+        Logger.NumerodePaso(12)
+        Logger.step('Hacer clic en el botón "Purchase" Ipad Mini')
+        PlaceOrderMetodos.clickOnPurchaseButton();
 
 
-         Logger.NumerodePaso(13)
-         Logger.step('Verificar que se muestra un mensaje de confirmación y se redirige al usuario a la página de inicio')
-         //GraciasporsucompraMetodos.verificabotonSuscess();
-         //GraciasporsucompraMetodos.clickOnButon();
-         
-         cy.wait(4000)
-         GraciasporsucompraMetodos.clickOnButon();
-         cy.wait(4000)
-         HomeMetodos.verificaMuetraPaginaHomePorLaURL();
+        Logger.NumerodePaso(13)
+        Logger.step('Verificar que se muestra un mensaje de confirmación y se redirige al usuario a la página de inicio Ipad MIni')
+        //GraciasporsucompraMetodos.verificabotonSuscess();
+        //GraciasporsucompraMetodos.clickOnButon();
+        
+        cy.wait(4000)
+        GraciasporsucompraMetodos.clickOnButon();
+        cy.wait(4000)
+        HomeMetodos.verificaMuetraPaginaHomePorLaURL();
 
-         Logger.postCondicion('Click en Log Out')          
-         CommonPageMetodos.logout();
+        Logger.postCondicion('Click en Log Out Ipad MIni')          
+        CommonPageMetodos.logout();
 
 
 
 
-      } )
-
-
-      
+     } )
 
 
 
